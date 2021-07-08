f2c87efb1d8217942fc9cdb368ab6f002161a7b8
Socheat Chhun <42795005+chhunsocheat@users.noreply.github.com>
2021-07-08 21:51:21 +1000

    Detailed Read me

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const navbar = document.querySelector('.nav-fixed');
-
+//when the position of Y axis is larger than 100, 
+//we add a new class else we remove them
 window.onscroll = () => {
   if (window.scrollY > 100) {
     navbar.classList.add('nav-active');
@@ -12,14 +13,12 @@ window.onscroll = () => {
 };
 
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-function show() {
-  mainMenu.style.display = 'flex';
-  mainMenu.style.top = '0';
-}
-
-function close() {
+openMenu.addEventListener('click', ()=> {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+  });
+closeMenu.addEventListener('click', ()=>{
   mainMenu.style.top = '-100%';
-}
+    
+});
+
